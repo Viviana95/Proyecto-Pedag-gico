5385c860698a7c70ee38950ed71f8b4e28ade524
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-09-20 14:47:48 +0200

    changes nav_home_admin

--- resources/views/components/nav_home_admin.blade.php
+++ resources/views/components/nav_home_admin.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <div>
-    <nav class="navbar navbar-expand-lg fondo">
+    <nav class="navbar navbar-expand-lg nav_color">
         <div class="container-fluid">
           <a class="navbar-brand" href="#"><img class="logo_factoria" src="{{ asset('./assets/logo_factoria_white.png')}}"></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +9,7 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto  mb-lg-0 ">
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-white mt-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown-toggle text-white mt-5 ms-5 me-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   LENGUAJES
                 </a>
                 <ul class="dropdown-menu">
@@ -19,10 +19,10 @@
                 </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white mt-5" href="#">AÑADIR RECURSOS</a>
+                <a class="nav-link text-white mt-5 me-5" href="#">AÑADIR RECURSOS</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link text-white mt-5" href="#">FORO</a>
+                <a class="nav-link text-white mt-5 me-5" href="#">FORO</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link text-white mt-5" href="#">ADMIN</a>
