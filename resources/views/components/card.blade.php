e8de53b43382606b129bc8fac198d74efab7db7c
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-21 22:01:56 +0200

    card_prueba

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="card" style="width: 18rem;">
+        <h5 class="card-title">TITULO</h5>
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+
+          <a href="#" class="btn btn-primary">EDIT</a>
+          <a href="#" class="btn btn-primary">DELETE</a>
+        </div>
+      </div>
+</div>
