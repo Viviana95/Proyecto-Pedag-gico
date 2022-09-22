cbde013e204692aa9cda567e2d6fdf5f98e07c01
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-09-22 12:12:26 +0200

    update panel_admin

--- resources/views/components/div_panel_admin.blade.php
+++ resources/views/components/div_panel_admin.blade.php
@@ -1,18 +1,18 @@
-<link rel="stylesheet" href="{{ asset('css/panel_admin.css') }}">
-<div>
-    <h1 class="mt-2 ms-2">Panel Administrador</h1>
 
-    <div class="d-flex justify-content-center">
+<div>
+    <h1 class="mt-5 mb-5">Panel Administrador</h1>
+
+    <div class="panel_item">
         <div>
-            <h3 class="d-flex justify-content-center">Recursos subidos</h3>
+            <h3>Recursos subidos</h3>
             <img class="imagen_panel" src="{{ asset('./assets/archivo.png')}}">
         </div>
         <div>
-            <h3 class="d-flex justify-content-center">Usuarios</h3>
+            <h3>Usuarios</h3>
             <img class="imagen_panel" src="{{ asset('./assets/grupo.png')}}">
         </div>
         <div>
-            <h3 class="d-flex justify-content-center">Registrar nuevo usuario</h3>
+            <h3>Registrar nuevo usuario</h3>
             <img class="imagen_panel" src="{{ asset('./assets/usuario.png')}}">
         </div>
     </div>
