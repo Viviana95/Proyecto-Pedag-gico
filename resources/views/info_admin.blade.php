53d8b6669a463653401f28c5405c0cd3ff109228
Tania <taniacsif@gmail.com>
2022-09-22 15:12:22 +0200

    m

--- resources/views/info_admin.blade.php
+++ resources/views/info_admin.blade.php
@@ -2,8 +2,10 @@
 <x-app-layout>
     <x-slot name="header">
         <x-nav_home_admin></x-nav_home_admin>
-        <h1>FACTORIA F5</h1>
-        <x-resources_admin_img></x-resources_admin_img>
+        <div class="container-img-admin">
+            <x-resources_admin_img></x-resources_admin_img>
+            <h1>FACTORIA F5</h1>
+    </div>
         <x-mine-resources></x-mine-resources>
         <a  class="btn btn-warning" href="{{route('means.index')}}">ENTRAR</a>
     </x-slot>
