6ffc67526c6a5ce3e79d086a100466a2342ac95d
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-25 21:19:17 +0200

    form-create-styles

--- resources/views/components/resources_admin_img.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container_img_admin">
-<div class="img_admin">
-
-</div>
-<input
-class="btn_select_file"
-name="image" type='file'>
-</div>
-
--- resources/views/info_admin.blade.php
+++ resources/views/info_admin.blade.php
@@ -1,14 +1,14 @@
 @include('layouts.layout')
+<div class="info-admin">
 <x-app-layout>
     <x-slot name="header">
         <x-nav_home_admin></x-nav_home_admin>
-        <div class="container-img-admin">
-            <x-resources_admin_img></x-resources_admin_img>
-            <h1>FACTORIA F5</h1>
-    </div>
-        <x-mine-resources></x-mine-resources>
-        <a  class="btn btn-warning" href="{{route('means.index')}}">ENTRAR</a>
-    </x-slot>
-</x-app-layout>
 
+            <x-mine-resources></x-mine-resources>
+            <a class="btn btn_orange" href="{{route('means.index')}}">ENTRAR</a>
+        </x-slot>
+    </div>
+    <x-footer></x-footer>
+</x-app-layout>
+</div>
 
