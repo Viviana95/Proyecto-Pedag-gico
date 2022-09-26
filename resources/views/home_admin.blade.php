88935ec55252c0ed0fdaa609bbf225216cf65836
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-26 18:11:23 +0200

    app layouts

--- resources/views/home_admin.blade.php
+++ resources/views/home_admin.blade.php
@@ -1,8 +1,10 @@
-@include('layouts.layout')
-
-<div class="home_admin_container">
-    <x-nav_home_admin></x-nav_home_admin>
-    <h1 class="masRecientes">MAS RECIENTES</h1>
-    <x-card></x-card>
-    <x-footer></x-footer>
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <div class="home_admin_container">
+            <x-nav_home_admin></x-nav_home_admin>
+            <h1 class="masRecientes">MAS RECIENTES</h1>
+            <x-card></x-card>
+            <x-footer></x-footer>
+        </div>
+    </x-slot>
+</x-app-layout>
