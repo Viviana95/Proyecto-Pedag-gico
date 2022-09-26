2d6c6e400379cf58b6567ed08d75c013eda70b7a
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-26 15:07:18 +0200

    add-img

--- resources/views/components/div_panel_admin.blade.php
+++ resources/views/components/div_panel_admin.blade.php
@@ -1,19 +1,19 @@
 <link rel="stylesheet" href="{{ asset('css/panel_admin.css') }}">
-<div>
-    <h1 class="mt-2 ms-2">Panel Administrador</h1>
+<div class="container_panel_admin">
+    <h1 class="title_panel_admin">Panel Administrador</h1>
 
-    <div class="d-flex justify-content-center">
-        <div>
-            <h3 class="d-flex justify-content-center">Recursos subidos</h3>
+    <div class="d-flex justify-content-center panel_admin">
+        <div class="option-admin">
+             <h3 class="title-option-admin">Recursos subidos</h3>
             <img class="imagen_panel" src="{{ asset('./assets/archivo.png')}}">
         </div>
-        <div>
-            <h3 class="d-flex justify-content-center">Usuarios</h3>
+        <div class="option-admin">
+            <h3 class="title-option-admin">Usuarios</h3>
             <img class="imagen_panel" src="{{ asset('./assets/grupo.png')}}">
         </div>
-        <div>
-            <h3 class="d-flex justify-content-center">Registrar nuevo usuario</h3>
+        <div class="option-admin">
+            <h3 class="title-option-admin">Registrar nuevo usuario</h3>
             <img class="imagen_panel" src="{{ asset('./assets/usuario.png')}}">
         </div>
     </div>
-</div>  
+</div>
--- resources/views/panel_admin.blade.php
+++ resources/views/panel_admin.blade.php
@@ -1,5 +1,8 @@
- @include('layouts.layout')
-
-<x-nav_home_admin></x-nav_home_admin>
-<x-div_panel_admin></x-div_panel_admin>
-<x-footer></x-footer>
+@include('layouts.layout')
+<x-app-layout>
+    <x-slot name="header">
+        <x-nav_home_admin></x-nav_home_admin>
+        <x-div_panel_admin></x-div_panel_admin>
+        <x-footer></x-footer>
+    </x-slot>
+</x-app-layout>
