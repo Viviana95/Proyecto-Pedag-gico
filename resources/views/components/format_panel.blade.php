8ab3389ce2d2eccb1976d6a547fea17975aaf95b
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-29 11:39:34 +0200

    format_panel

---
+++ resources/views/components/format_panel.blade.php
@@ -0,0 +1,24 @@
+
+
+<div class="container_format">
+    <h1 class="title">Elige el formato</h1>
+
+    <div class="d-flex justify-content-center panel_admin">
+        <div class="option-admin">
+             <h3 class="subtitle">Vídeo</h3>
+             <a href="#"><img class="imagen_panel" src="{{ asset('./assets/archivo.png')}}"></a>
+        </div>
+        <div class="option-admin">
+            <h3 class="subtitle">Link</h3>
+            <a href="{{ route('users.users') }}"><img class="imagen_panel" src="{{ asset('./assets/grupo.png')}}"></a>
+        </div>
+        <div class="option-admin" href="">
+            <h3 class="subtitle">Documento</h3>
+
+            <a href="#" class="ml-6 text-sl font-bold text_login"><img class="imagen_panel" src="{{ asset('./assets/usuario.png')}}"></a>
+
+        </div>
+    </div>
+</div>
+
+
