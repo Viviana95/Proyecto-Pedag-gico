c8ebda3c0bdcb68b2101c7e7d8fbb508244dc0d1
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-29 12:31:34 +0200

    n

--- resources/views/components/format_panel.blade.php
+++ resources/views/components/format_panel.blade.php
@@ -1,12 +1,10 @@
-
-
 <div class="container_format">
     <h1 class="title">Elige el formato</h1>
 
     <div class="d-flex justify-content-center panel_admin">
         <div class="option-admin">
-             <h3 class="subtitle">Vídeo</h3>
-             <a href="#"><img class="imagen_panel" src="{{ asset('./assets/archivo.png')}}"></a>
+            <h3 class="subtitle">Vídeo</h3>
+            <a href="#"><img class="imagen_panel" src="{{ asset('./assets/archivo.png')}}"></a>
         </div>
         <div class="option-admin">
             <h3 class="subtitle">Link</h3>
@@ -15,10 +13,9 @@
         <div class="option-admin" href="">
             <h3 class="subtitle">Documento</h3>
 
-            <a href="#" class="ml-6 text-sl font-bold text_login"><img class="imagen_panel" src="{{ asset('./assets/usuario.png')}}"></a>
+            <a href="#" class="ml-6 text-sl font-bold text_login"><img class="imagen_panel"
+                    src="{{ asset('./assets/usuario.png')}}"></a>
 
         </div>
     </div>
 </div>
-
-
