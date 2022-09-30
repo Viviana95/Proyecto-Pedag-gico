ed2088898477f43d2a7e7d096a3e60ba88515b6f
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-09-30 14:45:16 +0200

    form-create2

--- resources/views/components/form_create.blade.php
+++ resources/views/components/form_create.blade.php
@@ -17,28 +17,11 @@
                             <option value="Php">Php</option>
                             <option value="Javascript">Javascript</option>
                         </select>
-                        <label for="floatingInput"></label>
-                    </div>
-                    <p class="text-form">Selecciona imagen</p>
-                    <div class="form-floating mb-3">
-                        <input type="file" name="image" class="form-control" id="floatingInput">
-                        <label for="floatingInput"></label>
-                    </div>
-                    <p class="text-form">Selecciona formato</p>
-                    <div class="form-floating mb-3">
-                        <select name="format" class="form-select" aria-label="Default select example">
-                            <option selected>Open this select menu </option>
-                            <option value="Pdf">Pdf</option>
-                            <option value="Video">Video</option>
-                            <option value="Enlace">Enlace</option>
-                        </select>
-                        <label for="floatingInput"></label>
                     </div>
                     <p class="text-form">Añade Archivo o Enlace</p>
                     <div class="form-floating mb-3">
-                        <input type="file" name="file" class="form-control" id="floatingInput"
-                            placeholder="">
-                        <label for="floatingInput">FILE</label>
+                        <input type="text" name="file" class="form-control" id="floatingInput" placeholder="">
+
                     </div>
                     <div class="btn_container">
                         <button class="btn btn_orange" type="submit" href="{{route('means.index')}}">Añadir</button>
