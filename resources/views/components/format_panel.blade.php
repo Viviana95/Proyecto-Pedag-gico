642b073266a8e68e0f6358e6bc7dde041b510339
Nerea <fernandezcuestanerea@gmail.com>
2022-09-30 11:16:35 +0200

    images formats

--- resources/views/components/format_panel.blade.php
+++ resources/views/components/format_panel.blade.php
@@ -4,17 +4,16 @@
     <div class="d-flex justify-content-center panel_admin">
         <div class="option-admin">
             <h3 class="subtitle">Vídeo</h3>
-            <a href="{{ route('means.create') }}"><img class="imagen_panel" src="{{ asset('./assets/archivo.png')}}"></a>
+            <a href="{{ route('means.create') }}"><img class="imagen_panel" src="{{ asset('./assets/play.png')}}"></a>
         </div>
         <div class="option-admin">
             <h3 class="subtitle">Link</h3>
-            <a href="{{ route('means.create') }}"><img class="imagen_panel" src="{{ asset('./assets/grupo.png')}}"></a>
+            <a href="{{ route('means.create') }}"><img class="imagen_panel" src="{{ asset('./assets/enlace.png')}}"></a>
         </div>
         <div class="option-admin" href="">
             <h3 class="subtitle">Documento</h3>
-
             <a href="{{ route('means.create') }}" class="ml-6 text-sl font-bold text_login"><img class="imagen_panel"
-                    src="{{ asset('./assets/usuario.png')}}"></a>
+                    src="{{ asset('./assets/archivo.png')}}"></a>
 
         </div>
     </div>
