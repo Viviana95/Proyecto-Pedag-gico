defc4f6f5540f4c6a1ea39bf137194f5dbdc0b19
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-10-06 18:20:39 +0200

    progressbar

--- resources/views/components/progress_bar.blade.php
+++
@@ -1,8 +0,0 @@
-
-        <div style="width: 300px"  class="progress">
-            <div role="progress-bar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="50" style="width: 80px"
-                class="progress-bar"></div>
-        </div>
-
-
-
---
+++ resources/views/components/progressbar.blade.php
@@ -0,0 +1,9 @@
+
+    <div class="progress">
+        <div role="progress-bar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="50" style="width: 80px"
+            class="progress-bar">uuuuuuu</div>
+    </div>
+
+
+
+
