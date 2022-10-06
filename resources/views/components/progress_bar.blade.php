49707261ba331052ab234fe18cbb3ba596c4f340
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-10-06 10:30:49 +0200

    progress-bar

---
+++ resources/views/components/progress_bar.blade.php
@@ -0,0 +1,8 @@
+
+        <div style="width: 300px"  class="progress">
+            <div role="progress-bar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="50" style="width: 80px"
+                class="progress-bar"></div>
+        </div>
+
+
+
--- resources/views/components/resources_admin_img.blade.php
+++ resources/views/components/resources_admin_img.blade.php
@@ -1,8 +1,11 @@
 <div class="container_img_admin">
 <div class="img_admin">
+    <input
+    class="btn_select_file"
+    name="image" type='file'>
 </div>
-<input
-class="btn_select_file"
-name="image" type='file'>
+<div class="progress_bar_container">
+<x-progress_bar></x-progress_bar>
+</div>
 </div>
 
