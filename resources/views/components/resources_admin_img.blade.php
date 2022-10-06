d80f0166e223bae92ca635eb8af5954fa59c1586
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-10-06 13:17:38 +0200

    style my resources table

--- resources/views/components/resources_admin_img.blade.php
+++ resources/views/components/resources_admin_img.blade.php
@@ -1,33 +1,25 @@
-
- {{--  <div class="container_img_admin">
-    <div class="text-name">{{ Auth::user()->name }}</div>
-    <img class="img_admin" src="{{ Auth::user()->avatar }}" alt="User Profile Picture">
-    <input class="btn_select_file" name="image" type='file'>
-  </div> --}}
-  <div class="d-flex profile_container">
+<div class="d-flex profile_container">
     <div class="card border d-flex shadow mb-5 bg-body rounded">
       <h5 class="card-header text-name">{{ Auth::user()->name }}</h5>
       <div>
         <img class="img_admin rounded-circle" src="{{ Auth::user()->avatar }}" alt="User Profile Picture">
         <input class="mb-3 btn_select_file" name="image" type='file'>
         {{-- <input type="file" name="file" class="form-control btn_select_file" id="floatingInput" placeholder=""> --}}
-        
       </div>
     </div>
-
     
-      
-        <table class="table table-hover table-striped d-flex justify-content-start my_resources">
+    <div class="justify-content-start resources_container"> 
+        <table class="table table-hover table-striped">
           <thead>
               <tr>
-                <th scope="col">Title</th>
+                <th scope="col">Título</th>
                 {{-- <th scope="col">Image</th> --}}
-                <th scope="col">Language</th>
-                <th scope="col">Format</th>
+                <th scope="col">Lenguaje</th>
+                <th scope="col">Formato</th>
                 {{-- <th scope="col">File</th> --}}
-                <th scope="col">Action</th>
+                <th scope="col">Acción</th>
               </tr>
             </thead>
       </table>
-    
+    </div>
 </div>
