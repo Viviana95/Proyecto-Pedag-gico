fc759be7170ed39b2abb79ca6d32a2162212969e
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-10-10 08:51:26 +0200

    Format Controller

--- app/Http/Controllers/FormatController.php
+++ app/Http/Controllers/FormatController.php
@@ -6,5 +6,26 @@ use Illuminate\Http\Request;
 
 class FormatController extends Controller
 {
-    
+    public function index(Format $format)
+    {
+        return view('format_view', compact('format'));
+    }
+
+    public function video (Format $format)
+    {
+        $format = Format::find(2);
+        return route('means.create', ['format' => $format]);
+    }
+
+    public function link (Format $format)
+    {
+        $format = Format::find(1);
+        return route('means.create', ['format' => $format]);
+    }
+
+    public function file (Format $format)
+    {
+        $format = Format::find(3);
+        return route('means.create', ['format' => $format]);
+    }
 }
