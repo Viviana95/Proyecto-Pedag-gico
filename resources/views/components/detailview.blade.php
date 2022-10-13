908eff888139a9bd4366e404ddd27903bc1915b6
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-10-13 15:43:48 +0200

    details

--- resources/views/components/detailview.blade.php
+++ resources/views/components/detailview.blade.php
@@ -1,6 +1,5 @@
-<div class="">
-    <h1>{{$mean->title}}</h1>
-    <iframe width="100%" height="500px" src="{{asset('storage/' .$mean->file)}}"></iframe>
-    
+<div class="detail">
+    <h1 class="title mb-3">{{$mean->title}}</h1>
+    <iframe width="70%" height="800vh" src="{{asset('storage/' .$mean->file)}}"></iframe>
     </div>
   </div>
