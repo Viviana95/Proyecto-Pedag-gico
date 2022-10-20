bfae4e08ab8a902b008bd19a7b917fe219f14d73
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-10-20 15:57:49 +0200

    style foro

--- resources/views/components/foro.blade.php
+++ resources/views/components/foro.blade.php
@@ -1,3 +1,7 @@
 <div class="foro_container">
-    <img class="foro_image" src="{{ asset('./assets/foro.jpeg')}}" >
+    <img class="foro_image" src="{{ asset('./assets/foro.jpeg')}}">
+    <div class="foro_text">
+        <h1>¡BIENVENIDOS AL FORO!</h1>
+        <button>Usuarios</button>
+    </div>
 </div>
