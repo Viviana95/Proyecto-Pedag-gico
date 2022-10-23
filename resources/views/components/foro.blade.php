74ebaca0a4d0c6a5db6427f3b777328edd31fb6c
Cristina Sanchez <c.sanchez.cadrecha@outlook.es>
2022-10-23 13:37:53 +0200

    style foro

--- resources/views/components/foro.blade.php
+++ resources/views/components/foro.blade.php
@@ -2,7 +2,7 @@
     <div class="welcome" >
         <img class="img-fluid" src="{{ asset('./assets/construction1.jpg')}}">
         <h1 class="foro_text">¡BIENVENID@S AL FORO!</h1>
-        <button type="button" class="foro_btn p-2 shadow-sm  rounded">Entrar</button>
+        <button type="button" class="foro_btn p-2 shadow rounded">Entrar</button>
     </div>
-    <img class="foro_image" src="{{ asset('./assets/foro1.jpeg')}}">
+    <img class="foro_image" src="{{ asset('./assets/foro.jpeg')}}">
 </div>
